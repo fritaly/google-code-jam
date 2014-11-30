4176895d6aeac460557b71435c311b4b126da751
Francois Ritaly <francois.ritaly@gmail.com>
2014-11-30 17:45:33 +0100

    Generalised and simplified solution for SnapperChain. The solution now works with the large input

--- src/main/java/com/github/fritaly/googlecodejam/y2010/SnapperChain.java
+++ src/main/java/com/github/fritaly/googlecodejam/y2010/SnapperChain.java
@@ -1,87 +1,37 @@
 package com.github.fritaly.googlecodejam.y2010;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 import com.github.fritaly.googlecodejam.AbstractPuzzler;
 
 public class SnapperChain extends AbstractPuzzler {
 
-	private static class Snapper {
-		// true=ON, false=OFF. Defaults to false
-		boolean state = false;
+	// Here's what happens with 4 snappers when snapping
 
-		Snapper previous, next;
+	// 0	OFF,OFF,OFF,OFF
+	// 1	ON,OFF,OFF,OFF
+	// 2	OFF,ON,OFF,OFF
+	// 3	ON,ON,OFF,OFF
+	// 4	OFF,OFF,ON,OFF
+	// 5	ON,OFF,ON,OFF
+	// 6	OFF,ON,ON,OFF
+	// 7	ON,ON,ON,OFF
+	// 8	OFF,OFF,OFF,ON
+	// 9	ON,OFF,OFF,ON
+	// 10	OFF,ON,OFF,ON
+	// 11	ON,ON,OFF,ON
+	// 12	OFF,OFF,ON,ON
+	// 13	ON,OFF,ON,ON
+	// 14	OFF,ON,ON,ON
+	// 15	ON,ON,ON,ON
+	// 16	OFF,OFF,OFF,OFF (same as 0)
+	// 17	ON,OFF,OFF,OFF (same as 1)
+	// 18	OFF,ON,OFF,OFF (same as 2)
+	// 19	ON,ON,OFF,OFF (same as 3)
+	// ...
 
-		void snap() {
-			if (isPowered()) {
-				if (next != null) {
-					// Propagate to the next snapper before switching the state
-					next.snap();
-				}
-
-				// Switch the state
-				this.state = !state;
-			}
-		}
-
-		boolean isPowered() {
-			if (previous == null) {
-				// The first snapper (the one with no previous) is always
-				// powered
-				return true;
-			}
-
-			// A snapper is powered if its previous snapper is also powered and
-			// in the ON state
-			return (previous.isPowered() && previous.state);
-		}
-	}
-
-	private static class Chain {
-
-		final LinkedList<Snapper> list = new LinkedList<>();
-
-		public Chain(int length) {
-			Snapper previous = null;
-
-			for (int i = 0; i < length; i++) {
-				final Snapper snapper = new Snapper();
-
-				if (previous != null) {
-					previous.next = snapper;
-					snapper.previous = previous;
-				}
-
-				previous = snapper;
-
-				list.add(snapper);
-			}
-		}
-
-		@Override
-		public String toString() {
-			final StringBuilder builder = new StringBuilder();
-
-			for (int i = 0; i < list.size(); i++) {
-				if (i != 0) {
-					builder.append(",");
-				}
-
-				builder.append(list.get(i).state ? "ON" : "OFF");
-			}
-
-			return builder.toString();
-		}
-
-		Snapper getLast() {
-			return list.getLast();
-		}
-
-		Snapper getFirst() {
-			return list.getFirst();
-		}
-	}
+	// We only need to snap (K % (2^N)), encode this number in binary and check
+	// whether every bit is set to determine the lamp status
 
 	public static void main(String[] args) throws Exception {
 		new SnapperChain().execute();
@@ -99,16 +49,18 @@ public class SnapperChain extends AbstractPuzzler {
 
 		log("# of snappers: " + numberOfSnappers);
 		log("# of snaps: " + numberOfSnaps);
+		log("# of snaps % (2^N): " + numberOfSnaps % (1 << numberOfSnappers));
 
-		final Chain chain = new Chain(numberOfSnappers);
+		final int n = numberOfSnaps % (1 << numberOfSnappers);
 
-		log(chain);
+		String string = Integer.toBinaryString(n);
 
-		for (int i = 0; i < numberOfSnaps; i++) {
-			chain.getFirst().snap();
-			log(chain);
+		if (string.length() < numberOfSnappers) {
+			string = string + repeat("0", numberOfSnappers - string.length());
 		}
 
-		return chain.getLast().isPowered() && chain.getLast().state ? "ON" : "OFF";
+		log(string);
+
+		return repeat("1", numberOfSnappers).equals(string) ? "ON" : "OFF";
 	}
 }
