64dce0f24c43a4ee5df4160dd81346f510991463
Francois Ritaly <francois.ritaly@gmail.com>
2014-12-13 00:23:57 +0100

    Added solution for BabyHeight

---
+++ src/main/java/com/github/fritaly/googlecodejam/y2013/BabyHeight.java
@@ -0,0 +1,120 @@
+package com.github.fritaly.googlecodejam.y2013;
+
+import java.io.LineNumberReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.github.fritaly.googlecodejam.AbstractPuzzler;
+
+public class BabyHeight extends AbstractPuzzler {
+
+	private static final Height FIVE_INCHES = new Height("0'5\"");
+
+	private static final Height FOUR_INCHES = new Height("0'4\"");
+
+	private static class Height {
+		int feet, inches;
+
+		public Height() {
+		}
+
+		public Height(String text) {
+			final Matcher matcher = Pattern.compile("^([0-9]+)'([0-9]+)\"$").matcher(text);
+
+			if (!matcher.matches()) {
+				throw new IllegalArgumentException();
+			}
+
+			this.feet = Integer.parseInt(matcher.group(1));
+			this.inches = Integer.parseInt(matcher.group(2));
+		}
+
+		Height plus(Height other) {
+			final Height result = new Height();
+			result.feet = this.feet + other.feet;
+			result.inches = this.inches + other.inches;
+
+			if (result.inches >= 12) {
+				result.inches -= 12;
+				result.feet++;
+			}
+
+			return result;
+		}
+
+		Height minus(Height other) {
+			final Height result = new Height();
+			result.feet = this.feet - other.feet;
+			result.inches = this.inches - other.inches;
+
+			if (result.inches < 0) {
+				result.inches += 12;
+				result.feet--;
+			}
+
+			return result;
+		}
+
+		Height half(boolean lower) {
+			int totalInches = (this.feet * 12 + this.inches);
+
+			if (totalInches % 2 == 1) {
+				if (lower) {
+					// Round up to shrink the range
+					totalInches++;
+				} else {
+					// Round down to shrink the range
+					totalInches--;
+				}
+			}
+
+			final Height result = new Height();
+			result.feet = (totalInches / 2) / 12;
+			result.inches = (totalInches / 2) % 12;
+
+			return result;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%d'%d\"", feet, inches);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		new BabyHeight().run();
+	}
+
+	@Override
+	protected boolean isLogEnabled() {
+		return true;
+	}
+
+	protected List<String> getInputs() {
+		return Arrays.asList(String.format("%s-sample.in", getName()), String.format("%s-small-practice.in", getName()));
+	}
+
+	@Override
+	protected String solve(LineNumberReader reader) throws Exception {
+		final String[] array = reader.readLine().split(" ");
+
+		final boolean girl = "G".equals(array[0]);
+		final Height motherHeight = new Height(array[1]);
+		final Height fatherHeight = new Height(array[2]);
+
+		final Height result;
+
+		if (girl) {
+			result = new Height().plus(motherHeight).plus(fatherHeight).minus(FIVE_INCHES);
+		} else {
+			result = new Height().plus(motherHeight).plus(fatherHeight).plus(FIVE_INCHES);
+		}
+
+		final Height lowerBound = result.half(true).minus(FOUR_INCHES);
+		final Height upperBound = result.half(false).plus(FOUR_INCHES);
+
+		return String.format("%s to %s", lowerBound, upperBound);
+	}
+}
